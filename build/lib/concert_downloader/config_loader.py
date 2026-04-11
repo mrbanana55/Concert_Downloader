5838aa07598cfea0a6a8747b32222d44c476f25d
mrbanana55 <136206940+mrbanana55@users.noreply.github.com>
2026-04-10 19:40:12 -0600

    Delete build/lib/concert_downloader directory

--- build/lib/concert_downloader/config_loader.py
+++
@@ -1,7 +0,0 @@
-from concert_downloader.models import Concert
-import json
-
-def load_concert(path: str) -> Concert:
-    with open(path) as f:
-        data = json.load(f)
-    return Concert(**data)
--- build/lib/concert_downloader/errors.py
+++
@@ -1,4 +0,0 @@
-class ConcertError(Exception):
-    def __init__(self, message, file=None):
-        self.file = file
-        super().__init__(message)
